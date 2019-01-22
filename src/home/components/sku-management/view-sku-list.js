a809a1de517687986a444182a6ef026325a19f3c
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2019-01-22 11:56:11 +0530

    Added switch to update sku status in sku listing

--- src/home/components/sku-management/view-sku-list.js
+++ src/home/components/sku-management/view-sku-list.js
@@ -6,12 +6,12 @@ import {
   TableHeaderColumn,
   TableRow,
   TableRowColumn,
-  Switch
   //Scrollbars
 } from 'material-ui/Table'
 
 import RaisedButton from 'material-ui/RaisedButton'
 import FlatButton from 'material-ui/FlatButton'
+import Toggle from 'material-ui/Toggle'
 import CircularProgress from 'material-ui/CircularProgress'
 import { NavLink } from 'react-router-dom'
 import TableLoadingShell from './../table-loading-shell'
@@ -22,9 +22,9 @@ const TableHeaderItems = [
   'SKU_ID',
   'BRAND NAME',
   'VOLUME',
-  'STATUS',
+  // 'STATUS',
   'CREATED AT',
-  ''
+  'STATUS'
 ]
 
 const styles = [
@@ -32,100 +32,110 @@ const styles = [
   { width: '60px' },
   { width: '120px' },
   { width: '60px' },
-  { width: '60px' },
-  { width: '200px' },
+  // { width: '60px' },
+  { width: '100px' },
   { width: '60px' }
 ]
 
-function handleClick(e, item, props) {
-  let queryObj = {}
-  
-  // if(props.navigateTo !== "editSKU" && !item.is_active) {
-  //   e.preventDefault()
-  // } 
-  if(props.navigateTo === "editSKU") {
-    queryObj = {
-      brand_id: item.brand_id,
-      sku_id: item.sku_id,
-      is_active: item.is_active,
-      volume: item.volume,
-      image_url: item.image_url,
-      high_res_image: item.high_res_image,
-      low_res_image: item.low_res_image
-    }
-    props.history.push(`/home/manage-sku/edit/${item.brand_name}?sku_id=${item.id}&brand_id=${item.brand_id}`, queryObj) 
-  } else {
-    queryObj = {
-      brand_id: item.brand_id,
-      brand_name: item.brand_name,
-      sku_pricing_id: item.sku_pricing_id,
-      is_active: item.is_active,
-      volume: item.volume
-    }
-    props.history.push(`/home/sku-mapping/${item.id}`, queryObj)
-  }
-}
+class ViewSKUList extends React.Component {
 
-function ViewSKUList(data) {
-  return (
-    <React.Fragment>
-      <Table
-        className="bordered--table"
-        selectable={false}
-        fixedHeader
-      >
-        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-          <TableRow>
-            {
-              TableHeaderItems.map((item, i) => <TableHeaderColumn style={styles[i]} key={`table-head-col-${i}`}>{item}</TableHeaderColumn>)
-            }
-          </TableRow>
-        </TableHeader>
-        <TableBody
-          displayRowCheckbox={false}
-          showRowHover
+  constructor() {
+    super()
+  }
+
+  handleClick(e, item) {
+    let queryObj = {}
+    
+    // if(props.navigateTo !== "editSKU" && !item.is_active) {
+    //   e.preventDefault()
+    // } 
+    if(this.props.navigateTo === "editSKU") {
+      queryObj = {
+        brand_id: item.brand_id,
+        sku_id: item.sku_id,
+        is_active: item.is_active,
+        volume: item.volume,
+        image_url: item.image_url,
+        high_res_image: item.high_res_image,
+        low_res_image: item.low_res_image
+      }
+      this.props.history.push(`/home/manage-sku/edit/${item.brand_name}?sku_id=${item.id}&brand_id=${item.brand_id}`, queryObj) 
+    } else {
+      queryObj = {
+        brand_id: item.brand_id,
+        brand_name: item.brand_name,
+        sku_pricing_id: item.sku_pricing_id,
+        is_active: item.is_active,
+        volume: item.volume
+      }
+      this.props.history.push(`/home/sku-mapping/${item.id}`, queryObj)
+    }
+  }
+
+  updateSKUStatus(e, isInputClicked) {
+    console.log("Is input clicked", isInputClicked)
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Table
+          className="bordered--table"
+          selectable={false}
+          fixedHeader
         >
-          {
-            !data.loadingSkuList
-              ? (
-                data.skuList.map((item, i) => {
-                  return (
-                    <TableRow key={i}>
-                      <TableRowColumn style={styles[0]}>
-                        {
-                          <FlatButton primary label={`${data.navigateTo === "editSKU" ? "EDIT" : "VIEW"}`}
-                            onClick={(e) => handleClick(e, item, data)} 
-                            // style={ data.navigateTo !== 'editSKU' && !item.is_active ? {opacity: 0.55} : {}}  
-                          /> 
-                        }
-                      </TableRowColumn>
-                      <TableRowColumn style={styles[1]}>{item.id}</TableRowColumn>
-                      <TableRowColumn style={styles[2]}>{item.brand_name}</TableRowColumn>
-                      <TableRowColumn style={styles[3]}>{item.volume}</TableRowColumn>
-                      <TableRowColumn style={styles[4]}>{item.is_active ? 'ACTIVE' : 'INACTIVE'}</TableRowColumn>
-                      <TableRowColumn style={styles[5]}>{item.created_at}</TableRowColumn>
-                      <TableRowColumn style={styles[6]}>
-                        <Switch
-                          checked={item.is_active}
-                          //onChange={this.handleChange('checkedB')}
-                          //value="checkedB"
-                          color="primary"
-                        />
-                      </TableRowColumn>
-                    </TableRow>
-                  )
-                })
-              )
-              : (
-                [1, 2, 3, 4, 5].map(() => (
-                  <TableLoadingShell />
-                ))
-              )
-          }
-        </TableBody>
-      </Table>
-    </React.Fragment>
-  )
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow>
+              {
+                TableHeaderItems.map((item, i) => <TableHeaderColumn style={styles[i]} key={`table-head-col-${i}`}>{item}</TableHeaderColumn>)
+              }
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={false}
+            showRowHover
+          >
+            {
+              !this.props.loadingSkuList
+                ? (
+                  this.props.skuList.map((item, i) => {
+                    return (
+                      <TableRow key={i}>
+                        <TableRowColumn style={styles[0]}>
+                          {
+                            <FlatButton primary label={`${this.props.navigateTo === "editSKU" ? "EDIT" : "VIEW"}`}
+                              onClick={(e) => this.handleClick(e, item)} 
+                              // style={ data.navigateTo !== 'editSKU' && !item.is_active ? {opacity: 0.55} : {}}  
+                            /> 
+                          }
+                        </TableRowColumn>
+                        <TableRowColumn style={styles[1]}>{item.brand_id}</TableRowColumn>
+                        <TableRowColumn style={styles[2]}>{item.brand_name}</TableRowColumn>
+                        <TableRowColumn style={styles[3]}>{item.volume}</TableRowColumn>
+                        {/* <TableRowColumn style={styles[4]}>{item.is_active ? 'ACTIVE' : 'INACTIVE'}</TableRowColumn> */}
+                        <TableRowColumn style={styles[4]}>{item.created_at}</TableRowColumn>
+                        <TableRowColumn style={styles[5]}>
+                          <Toggle 
+                            toggled={item.is_active}
+                            onToggle={this.updateSKUStatus}
+                          />
+                        </TableRowColumn>
+                      </TableRow>
+                    )
+                  })
+                )
+                : (
+                  [1, 2, 3, 4, 5].map(() => (
+                    <TableLoadingShell />
+                  ))
+                )
+            }
+          </TableBody>
+        </Table>
+      </React.Fragment>
+    )
+  }
+  
 }
 
 export default ViewSKUList
